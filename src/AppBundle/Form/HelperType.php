15d336188fc76546fd138f8ec7ec20b59dbdc70e
valery <v.a.kiselyov@gmail.com>
2017-03-07 14:13:02 +0300

    Config tinymce

--- src/AppBundle/Form/HelperType.php
+++ src/AppBundle/Form/HelperType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,22 @@ class HelperType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('command')->add('description')->add('isActive')->add('deleted')->add('created_at')->add('updated_at');
+        $builder
+            ->add('title')
+            ->add('command', TextareaType::class, [
+                'attr' => [
+                    'class' => 'tinymce'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'tinymce'
+                ]
+            ])
+            ->add('isActive')
+            ->add('deleted')
+            ->add('created_at')
+            ->add('updated_at');
     }
     
     /**
