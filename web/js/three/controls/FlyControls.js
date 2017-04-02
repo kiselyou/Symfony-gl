22d9f141e1903d1d050479dae3a0ba597230100b
kiselyou <v.a.kiselyov@gmail.com>
2017-04-02 17:42:51 +0300

    FlyControl

--- web/js/three/controls/FlyControls.js
+++ web/js/three/controls/FlyControls.js
@@ -19,21 +19,12 @@ THREE.FlyControls = function ( object, camera, domElement ) {
 
 	this.speed = {
 		current: 0, // m.s Can not be less than zero. Default 0
-		max:  1000, // m.s It is maximum speed the model
+		max:  1500, // m.s It is maximum speed the model
 		min: - 500	// m.s If less than zero. The model is moving back
 	};
 
-	this.acceleration = 10; // m.s
-	this.deceleration = 10; // m.s
-
-	this.previousPosition = new THREE.Vector3(0, 0, 0);
-
-	/**
-	 * It is approximate position to motion
-	 *
-	 * @type {any}
-     */
-	this.positionTo = new THREE.Vector3();
+	this.acceleration = 20; // m.s
+	this.deceleration = 10;  // m.s
 
 	var scope = this;
 
@@ -44,17 +35,72 @@ THREE.FlyControls = function ( object, camera, domElement ) {
 
 	this.update = function () {
 
-		if ( orbitControl ) {
+	    if ( fly || scope.speed.current != 0 ) {
 
-			if ( this.object ) {
+			var positionTo = getPositionTo();
 
-				// orbitControl
-				// 	.stopMoveCamera()
-				// 	.moveCameraTo( this.object );
+            scope.object.lookAt( positionTo );
+
+	    	if ( motion.direct && scope.speed.current < scope.speed.max ) {
+
+				scope.speed.current += scope.acceleration;
 			}
 
-			orbitControl.update();
-		}
+			if ( motion.direct && scope.speed.current > scope.speed.max ) {
+
+				scope.speed.current = scope.speed.max;
+			}
+
+			if ( motion.backward && scope.speed.current > scope.speed.min ) {
+
+				scope.speed.current -= scope.deceleration;
+			}
+
+			if ( motion.backward && scope.speed.current < scope.speed.min ) {
+
+				scope.speed.current = scope.speed.min;
+			}
+
+			// Авто торможение
+			if ( !motion.direct && !motion.backward ) {
+
+				if ( scope.speed.current > scope.deceleration ) {
+
+					scope.speed.current -= scope.deceleration;
+
+				} else if ( scope.speed.current < - scope.deceleration ) {
+
+					scope.speed.current += scope.deceleration;
+
+				} else {
+
+					scope.speed.current = 0;
+				}
+			}
+
+            var a = positionTo.x - this.object.position.x;
+            var b = positionTo.z - this.object.position.z;
+            var len = Math.sqrt( a * a + b * b ) * SCALE;
+
+            ox = a / len * this.speed.current;
+            oz = b / len * this.speed.current;
+
+			this.object.position.x += ox;
+			this.object.position.z += oz;
+
+        }
+
+            // if (orbitControl) {
+            //
+            //     if (this.object) {
+            //
+            //         orbitControl
+            //             .stopMoveCamera()
+            //             .moveCameraTo(this.object);
+            //     }
+            //
+            //     orbitControl.update();
+            // }
 	};
 
 	/**
@@ -69,14 +115,15 @@ THREE.FlyControls = function ( object, camera, domElement ) {
      */
 	this.initOrbitControl = function () {
 
-		orbitControl = new THREE.OrbitControls( scope.camera, scope.domElement );
-		orbitControl.mouseButtons = { ORBIT: THREE.MOUSE.RIGHT, ZOOM: THREE.MOUSE.MIDDLE, PAN: THREE.MOUSE.LEFT };
-		orbitControl.enablePan = false;
-		orbitControl.enableKeys = false;
-		orbitControl.minDistance = 30;
-		orbitControl.maxPolarAngle = 75 * Math.PI / 180;
-		orbitControl.maxDistance = 3000;
-		orbitControl.rotateSpeed = 3.0;
+        // orbitControl = new THREE.OrbitControls( scope.camera, scope.domElement );
+        // orbitControl.mouseButtons = { ORBIT: THREE.MOUSE.RIGHT, ZOOM: THREE.MOUSE.MIDDLE, PAN: THREE.MOUSE.LEFT };
+        // orbitControl.enablePan = false;
+        // orbitControl.enableKeys = false;
+        // orbitControl.minDistance = 30;
+        // orbitControl.maxPolarAngle = 75 * Math.PI / 180;
+        // orbitControl.maxDistance = 1000;
+        // orbitControl.rotateSpeed = 3.0;
+        // orbitControl.enabled = false;
 
 		return this;
 	};
@@ -87,13 +134,6 @@ THREE.FlyControls = function ( object, camera, domElement ) {
 	 * @returns {void}
      */
 	function motionControl() {
-
-		if ( motion.left && motion.right ) {
-
-			motion.left = false;
-			motion.right = false;
-
-		}
 
 		if ( motion.direct && motion.backward ) {
 
@@ -182,6 +222,76 @@ THREE.FlyControls = function ( object, camera, domElement ) {
 		}
 
 		return false;
+	}
+
+	/**
+	 *
+	 * @param {Vector3} a
+	 * @param {Vector3} b
+	 * @returns {number}
+	 */
+	function getAngle( a, b ) {
+
+        var v = new THREE.Vector3();
+		v.subVectors( b, a );
+		return Math.atan2( v.z, v.x );
+	}
+
+	/**
+	 * It is approximate position to motion
+	 *
+	 * @type {Vector3}
+	 */
+	var _positionTo = new THREE.Vector3();
+
+	/**
+	 *
+	 * @type {Vector3}
+	 * @private
+     */
+	var _prev = new THREE.Vector3( 0, 0, -1000 );
+
+    /**
+     *
+     * @type {number}
+     * @private
+     */
+    var _angle = getAngle( _prev, scope.object.position );
+
+	/**
+	 *
+     */
+	function getPositionTo() {
+
+	    if ( motion.direct && scope.speed.current > 0 ) {
+
+            if (motion.left) {
+                _angle -= 0.01;
+            }
+
+            if (motion.right) {
+                _angle += 0.01;
+            }
+        }
+
+        if ( motion.backward && scope.speed.current < 0 ) {
+
+            if (motion.left) {
+                _angle += 0.005;
+            }
+
+            if (motion.right) {
+                _angle -= 0.005;
+            }
+        }
+
+		var x = scope.object.position.x + scope.far * Math.cos( _angle );
+		var z = scope.object.position.z + scope.far * Math.sin( _angle );
+
+		_positionTo.setX( x );
+		_positionTo.setZ( z );
+
+		return _positionTo;
 	}
 
 	window.addEventListener( 'keydown', keyDown, false );
