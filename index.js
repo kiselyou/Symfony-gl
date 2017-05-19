b4c5a61900146a399678bc52b0cfdbce183c4687
kiselyou <v.a.kiselyov@gmail.com>
2017-05-20 01:59:00 +0300

    Update templates.

--- index.js
+++ index.js
@@ -6,30 +6,49 @@ var cheerio = require('cheerio');
 var config = require(__dirname + '/server/config/config.json');
 var express = require('express');
 var app = express();
-var appDir = getEnvironment(config);
 
-var DIR_ROUTES = __dirname + '/server/routing/';
 
-var DIR_TEMPLATES =  __dirname + '/' + appDir + '/templates/html/';
-var DIR_TEMPLATES_JS =  __dirname + '/' + appDir + '/templates/html/';
+// ADD TO CLASS REQUEST
+var DIR_APP = getEnvironment(config);
+var PATH_APP = __dirname + '/' + DIR_APP;
+
+var TEMPLATE_JS = '/js';
+var TEMPLATE_HTML = '/html';
+var DIR_TEMPLATES = '/patterns';
+var PATH_TEMPLATES = PATH_APP + DIR_TEMPLATES;
+var PATH_TEMPLATES_JS = PATH_TEMPLATES + TEMPLATE_JS;
+var PATH_TEMPLATES_HTML = PATH_TEMPLATES + TEMPLATE_HTML;
+
+var DIR_ROUTES = '/server/routing';
+var PATH_ROUTES = __dirname + DIR_ROUTES;
+
+// CREATE CLASS CACHE
+var cache = {
+    scripts: [],
+    patterns: [],
+    build: null
+};
 
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-fs.readdir(DIR_ROUTES, function (err, routes) {
+fs.readdir(PATH_ROUTES, function (err, routes) {
     var routeConfig = [];
     for ( var i = 0; i < routes.length; i++ ) {
-        routeConfig.push(require(DIR_ROUTES + routes[ i ]));
+        var path = concatPath(PATH_ROUTES, routes[ i ]);
+        routeConfig.push(require(path));
     }
 });
 
+// CREATE ROUTING CLASS
 app.get('/', function (req, res) {
-    fs.readFile(appDir + '/index.html', null, function (error, content) {
+    var path = concatPath(DIR_APP, '/index.html');
+    fs.readFile(path, null, function (error, content) {
         if (error) {
             if (error.code === 'ENOENT') {
-                fs.readFile(appDir + '/404.html', function (error, content) {
+                fs.readFile(DIR_APP + '/404.html', function (error, content) {
                     if (error) {
                         res.writeHead(500);
                         res.end(MESSAGE_SERVER + error.code + ' ..\n');
@@ -44,8 +63,16 @@ app.get('/', function (req, res) {
             }
         } else {
             res.writeHead(200, {'Content-Type': 'text/html'});
-            var template = scriptsFilter(includeTemplate(content));
-            res.end(template, config.encoding, true);
+
+
+            cache.scripts = [];
+            cache.patterns = [];
+            cache.build = null;
+
+            if (!cache.build) {
+                cache.build = loadScript(includePattern(content));
+            }
+            res.end(cache.build, config.encoding, true);
         }
     });
 });
@@ -55,20 +82,23 @@ app.get('/play', function (req, res) {
 });
 
 app.post('/template', function (req, res) {
-    fs.readFile(DIR_TEMPLATES + req.body['template'], null, function (error, content) {
+    var path = concatPath(PATH_TEMPLATES_HTML, req.body['template']);
+    fs.readFile(path, null, function (error, content) {
         if (error) {
             res.writeHead(500);
             res.end(MESSAGE_SERVER + error.code + ' ..\n');
         } else {
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(includeTemplate(content), config.encoding, true);
+            res.end(includePattern(content), config.encoding, true);
         }
     });
 });
 
-app.use(express.static(__dirname + '/' + appDir));
+app.use(express.static(PATH_APP));
 
 app.listen(config.port, config.host);
+
+// CREATE CLASS PATTERN
 
 /**
  * Get directory of environment
@@ -92,21 +122,23 @@ function getEnvironment(config) {
  * @param {boolean} extend
  * @returns {*}
  */
-function includeTemplate(content, extend) {
+function includePattern(content, extend) {
     var path;
     var $ = cheerio.load(content);
 
     if(extend) {
-        var str = extendTemplate($);
+        var str = extendPattern($);
         if (str) {
             return str;
         }
     }
 
     $('[data-include]').each(function() {
-        path = DIR_TEMPLATES + $(this).attr('data-include');
+        var nameTemplate = $(this).attr('data-include');
+        path = concatPath(PATH_TEMPLATES_HTML, nameTemplate);
         if (fs.existsSync(path)) {
-            $(this).replaceWith(includeTemplate(fs.readFileSync(path, {encoding: config.encoding}), true));
+            $(this).replaceWith(includePattern(fs.readFileSync(path, {encoding: config.encoding}), true));
+            cache.patterns.push(nameTemplate);
         } else {
             console.log('Include: Template was not found in path ' + path);
         }
@@ -121,7 +153,7 @@ function includeTemplate(content, extend) {
  * @param {cheerio} $ the template
  * @returns {boolean|string}
  */
-function extendTemplate($) {
+function extendPattern($) {
     $('[data-extend]').each(function () {
 
         var blockName = $(this).attr('data-block');
@@ -131,13 +163,15 @@ function extendTemplate($) {
             return false;
         }
 
-        path = DIR_TEMPLATES + $(this).attr('data-extend');
+        var nameTemplate = $(this).attr('data-extend');
+        path = concatPath(PATH_TEMPLATES_HTML, nameTemplate);
 
         if (fs.existsSync(path)) {
 
             var $$ = cheerio.load(fs.readFileSync(path, {encoding: config.encoding}));
             $$('[data-block="' + blockName + '"]').replaceWith($(this).children());
-            $(this).replaceWith(includeTemplate($$.html(), true));
+            $(this).replaceWith(includePattern($$.html(), true));
+            cache.patterns.push(nameTemplate);
             return $.html();
 
         } else {
@@ -150,13 +184,40 @@ function extendTemplate($) {
 
 /**
  *
- * @param {string} content
- * @return {*}
+ * @param {cheerio} $
+ * @return {void}
  */
-function scriptsFilter(content) {
-    var $ = cheerio.load(content);
+function scriptsFilter($) {
     $('script').each(function () {
         $('body:last-child').append($(this));
     });
+}
+
+/**
+ *
+ * @param {string} str
+ * @return {string}
+ */
+function loadScript(str) {
+    var $ = cheerio.load(str);
+    for (var i = 0; i < cache.patterns.length; i++) {
+        var nameTemplateScript = cache.patterns[i].replace(/(\.html)$/, '.js');
+        if (fs.existsSync(concatPath(PATH_TEMPLATES_JS, nameTemplateScript))) {
+            var path = concatPath(concatPath(DIR_TEMPLATES, TEMPLATE_JS), nameTemplateScript);
+            $('body:last-child').append('<script type="application/javascript" src="' + path.replace(/^(\/)/, '') + '"></script>');
+            cache.scripts.push(nameTemplateScript);
+        }
+    }
+    scriptsFilter($);
     return $.html();
+}
+
+/**
+ *
+ * @param {string} dir - possible value ( '/var/www/project/' | '/var/www/project/' )
+ * @param {string} str - possible value ( '/path/to/file' )
+ * @returns {string}
+ */
+function concatPath(dir, str) {
+    return dir.replace(/(\/)$/, '') + '/' + str.replace(/^(\/)/, '');
 }
