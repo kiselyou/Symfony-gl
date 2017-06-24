30e0a4480f015c7e7a1fbd9a3bc27dab3f6b9f4f
kiselyou <v.a.kiselyov@gmail.com>
2017-06-24 14:13:10 +0300

    Gulp create watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
@@ -7,10 +9,15 @@ var imagemin = require('gulp-imagemin');
 var del = require('del');
 var cache = require('gulp-cache');
 var runSequence = require('run-sequence');
+var path = require('path');
 
 gulp.task('useref', function(){
   	return gulp.src('app/*.html')
-		.pipe(useref())
+        .pipe(useref({
+            transformPath: function(filePath) {
+                return filePath.replace('/app', '')
+            }
+        }))
 		.pipe(gulpIf('*.js', uglify()))
 		.pipe(gulpIf('*.css', cssnano()))
 		.pipe(gulp.dest('dist'));
@@ -19,13 +26,12 @@ gulp.task('useref', function(){
 gulp.task('images', function(){
 	return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
 		.pipe(cache(imagemin({
+            progressive: true,
 			interlaced: true
 		})))
 		.pipe(gulp.dest('dist/images'));
 });
 
-//lessc app/less/config.less app/css/test.css
-//lessc app/less/build.less app/css/theme-iron-war.css
 gulp.task('move', function() {
 
 	gulp.src('app/*.ico')
@@ -52,21 +58,38 @@ gulp.task('move', function() {
 		.src('app/fonts/**/*')
 		.pipe(gulp.dest('dist/fonts'));
 
-	gulp
-		.src('app/patterns/**/*.js')
-		.pipe(gulp.dest('dist/patterns'));
+    gulp
+        .src('app/view/**/*')
+        .pipe(gulp.dest('dist/view'));
 
-	gulp
-		.src('app/patterns/**/*.html')
-		.pipe(gulp.dest('dist/patterns'));
+    gulp.src('dist/app/**/*')
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean:dist', function() {
 	return del.sync('dist');
 });
 
-gulp.task('build', function (callback) {
-	// runSequence('clean:dist', ['useref', 'images', 'fonts', 'models', 'icon'], callback);
-	runSequence('clean:dist', ['useref', 'images', 'move'], callback);
+gulp.task('clean:app', function() {
+    return del.sync('dist/app');
+});
+
+//lessc app/less/config.less app/css/test.css
+//lessc app/less/build.less app/css/theme-iron-war.css
+
+gulp.task('less', function() {
+    return gulp.src('./app/less/theme-iron-war.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less') ]
+        }))
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./app/less/**/*.less', ['less']);
+});
+
+gulp.task('build', function (done) {
+	runSequence('clean:dist', 'useref', 'move', 'images', 'clean:app', done);
 });
 
